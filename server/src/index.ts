c28bdfdff520f20b9db8a1a10f29b18c54a566d7
Petri M <petri.malmstrom@gmail.com>
2023-02-07 13:33:43 +0200

    creating decks / cards

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,24 +3,21 @@ import mongoose from 'mongoose';
 import { config } from 'dotenv'
 import Deck from "./models/Deck"
 import cors from "cors"
+import { createDeckController } from './controllers/createDeckController';
+import { getDecksController } from './controllers/getDecksController';
+import { deleteDeckController } from './controllers/deleteDeckController';
+import { createCardForDeckController } from './controllers/createCardForDeckController';
 config();
 const app = express()
 const port = 3000
+
 app.use(express.json())
 app.use(cors())
 
-app.get('/decks', async (req: Request, res: Response) =>{
-  const decks = await Deck.find()
-  res.json(decks)
-})
-
-app.post('/decks', async (req: Request, res: Response) => {
-    const newDeck = new Deck({
-    title: req.body.title
-  })
-  const createdDecl = await newDeck.save()
-  res.json(createdDecl)
-})
+app.get('/decks', getDecksController)
+app.post('/decks', createDeckController)
+app.delete('/decks/:deckId', deleteDeckController)
+app.post('/decks/:deckId/cards', createCardForDeckController)
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`Example app listening on port ${port}`)
