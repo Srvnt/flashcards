b0fb87dbfd196c0b7d1313d71148d2b9f8cd398d
Petri M <petri.malmstrom@gmail.com>
2023-02-07 09:59:47 +0200

    working on decks

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,17 @@ import express, { Request, Response} from 'express'
 import mongoose from 'mongoose';
 import { config } from 'dotenv'
 import Deck from "./models/Deck"
+import cors from "cors"
 config();
 const app = express()
 const port = 3000
 app.use(express.json())
+app.use(cors())
 
+app.get('/decks', async (req: Request, res: Response) =>{
+  const decks = await Deck.find()
+  res.json(decks)
+})
 
 app.post('/decks', async (req: Request, res: Response) => {
     const newDeck = new Deck({
